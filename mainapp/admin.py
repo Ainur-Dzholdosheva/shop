8843ee5ffdfbf753cfd03e4089afe206e86ff352
Ainur-Dzholdosheva <ajnurdzoldoseva@gmail.com>
2021-10-31 20:10:38 +0300

    ignore media files

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,27 +1,32 @@
 from django.contrib import admin
 from django import forms
+from django.utils.safestring import mark_safe
 from django.forms import ModelChoiceField, ModelForm, ValidationError
 from .models import *
-
 from PIL import Image
 
 
 class NotebookAdminForm(ModelForm):
 
     MIN_RESOLUTION=[400, 400]
+    MAX_RESOLUTION=[800, 800]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['image'].help_text='Загружайте изображения с минимальным разрешением {}x{}'.format(
+        self.fields['image'].help_text=mark_safe('<span style="color:red;">Загружайте изображения с минимальным разрешением {}x{}</span>'.format(
             *self.MIN_RESOLUTION
+        )
         )
     
     def clean_image(self):
         image=self.cleaned_data['image']
         img = Image.open(image)
         min_height, min_width=self.MIN_RESOLUTION
+        max_height, max_width=self.MAX_RESOLUTION
         if img.height < min_height or img.width < min_width:
             raise ValidationError('Разрешение изображения меньше минимального')
+        if img.height < max_height or img.width < max_width:
+            raise ValidationError('Разрешение изображения больше максимального')
 
         return image
 
